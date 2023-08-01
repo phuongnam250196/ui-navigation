9a197c0b58a043dd731fdb838f6127fbce251dd9
phuongnam250196 <namtp@regit-technology.com>
2023-08-01 23:17:26 +0700

    Add scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 /// Flutter code sample for [BottomNavigationBar].
@@ -28,22 +29,131 @@ class _BottomNavigationBarExampleState
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
+
+  final screen1 = const Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'Index 0: Home',
+        style: optionStyle,
+      ),
+      Text(
+        'Index 0: Home',
+        style: optionStyle,
+      )
+    ],
+  );
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverPadding(
+          padding: const EdgeInsets.all(20),
+          sliver: SliverGrid.count(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.red[300],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        topRight: Radius.circular(8.0)
+                      ),
+                      child: Image.asset(
+                        'assets/images/image1.jpg',
+                        height: 80,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Hello, I am Meo'),
+                      subtitle: Text('Que vinh phuc'),
+                      onTap: () {
+                        print('1');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[400],
+                child: const Text('Who scream'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[300],
+                child: const Text('Sound of screams but the'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[400],
+                child: const Text('Who scream'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[100],
+                child: const Text("He'd have you all unravel at the"),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[200],
+                child: const Text('Heed not the rabble'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[300],
+                child: const Text('Sound of screams but the'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[400],
+                child: const Text('Who scream'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[300],
+                child: const Text('Sound of screams but the'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[400],
+                child: const Text('Who scream'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[500],
+                child: const Text('Revolution is coming...'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green[600],
+                child: const Text('Revolution, they...'),
+              ),
+            ],
+          ),
+        ),
+      ],
     ),
-    Text(
+    const Text(
       'Index 1: Business',
       style: optionStyle,
     ),
-    Text(
+    const Text(
       'Index 2: School',
       style: optionStyle,
     ),
   ];
 
   void _onItemTapped(int index) {
+    print("hello: $index");
     setState(() {
       _selectedIndex = index;
     });
@@ -74,7 +184,7 @@ class _BottomNavigationBarExampleState
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.green[800],
         onTap: _onItemTapped,
       ),
     );
